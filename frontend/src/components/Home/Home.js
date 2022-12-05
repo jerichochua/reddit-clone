0dd1baffa9bd31b9bfb542753b0e66813265627b
Jericho Chua <jerichochua@gmail.com>
2022-12-05 09:24:14 +0800

    remove sidebar from home page in frontend

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import Content from './Content/Content';
-import Side from './Side/Side';
 import './Home.css';
 
 export default function Home(props) {
   return (
     <div className='home'>
       <Content />
-      <Side />
     </div>
   );
 }
