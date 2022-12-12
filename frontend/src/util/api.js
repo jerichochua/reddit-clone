8d800463dcc8372175679c7609e8bb8afd5625ad
Jericho Chua <jerichochua@gmail.com>
2022-12-12 15:32:27 +0800

    add post request function for frontend

--- frontend/src/util/api.js
+++ frontend/src/util/api.js
@@ -14,3 +14,19 @@ export const get = async (endpoint) => {
   }
   return data;
 };
+
+export const post = async (endpoint, body) => {
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  };
+  const response = await fetch(`${baseUrl}/${endpoint}`, options);
+  const data = await response.json();
+  if (!response.ok) {
+    throw new Error(data.message);
+  }
+  return data;
+};
