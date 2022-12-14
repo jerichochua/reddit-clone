01c0c3e5a99843461702e5f7cc3f4ba7be7d9d60
Jericho Chua <jerichochua@gmail.com>
2022-12-14 16:44:21 +0800

    implement commenting on a post
    at this point, it only shows a message if the submission is successful or not, it does not show the new comment

--- frontend/src/components/Post/CommentForm.js
+++ frontend/src/components/Post/CommentForm.js
@@ -1,16 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { post } from '../../util/api';
+import { useAppContext } from '../../contexts/AppProvider';
 import './CommentForm.css';
 
-const CommentForm = () => {
+const CommentForm = ({ postId }) => {
+  const { state } = useAppContext();
+  const [message, setMessage] = useState('');
+
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+    const body = { content: e.target.content.value };
+    try {
+      const response = await post(`posts/${postId}`, body, state.token);
+      if (response) {
+        setMessage('comment added');
+        e.target.content.value = '';
+      } else {
+        setMessage('error adding comment');
+        console.error(response);
+      }
+    } catch (error) {
+      setMessage('error adding comment');
+      console.error(error);
+    }
+  };
+
   return (
     <div className='comment-form-container'>
-      <form className='comment-form'>
+      <form className='comment-form' onSubmit={onSubmitForm}>
         <textarea
+          name='content'
           className='comment-form-textarea'
           placeholder='Add a comment...'
           rows='2'
+          required
         ></textarea>
-        <button className='comment-form-button'>Comment</button>
+        <div className='comment-form-footer'>
+          {message && <span className='comment-footer-message'>{message}</span>}
+          <button className='comment-form-button'>Comment</button>
+        </div>
       </form>
     </div>
   );
