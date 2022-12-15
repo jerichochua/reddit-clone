93bfd7aad74d2b1f6accd740325602739e0e5a71
Jericho Chua <jerichochua@gmail.com>
2022-12-15 10:24:50 +0800

    cosmetic tweak to username in header

--- frontend/src/components/Header/Username.js
+++ frontend/src/components/Header/Username.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Username = ({ username }) => {
-  return <div className='header-item'>logged in as {username}</div>;
+  return (
+    <div className='header-item-username'>
+      <Link className='header-link' to={`/user/${username}`}>
+        {username}
+      </Link>
+    </div>
+  );
 };
 
 export default Username;
