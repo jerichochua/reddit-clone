8d708241c9392c27ad4f6db524839bf180aa5880
Jericho Chua <jerichochua@gmail.com>
2022-12-19 10:13:49 +0800

    use async-await in users route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,7 +1,7 @@
 const pool = require('../db/db');
 const router = require('express').Router();
 
-router.get('/:username', (req, res) => {
+router.get('/:username', async (req, res) => {
   const username = req.params.username;
   query = `
     SELECT
@@ -18,12 +18,15 @@ router.get('/:username', (req, res) => {
     GROUP BY posts.id, users.username
     ORDER BY score DESC
   `;
-  pool.query(query, [username], (err, result) => {
-    if (err) {
-      res.status(500).send('Error retrieving posts');
+  try {
+    const result = await pool.query(query, [username]);
+    if (result.rows.length === 0) {
+      return res.status(404).send('User not found');
     }
-    res.send(result.rows);
-  });
+    res.status(200).send(result.rows);
+  } catch (err) {
+    res.status(500).send('Error retrieving posts');
+  }
 });
 
 module.exports = router;
