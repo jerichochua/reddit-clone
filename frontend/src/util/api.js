bd2f02619cc1626cfdfd13ff8d83f5b4cf6e8e6b
Jericho Chua <jerichochua@gmail.com>
2022-12-19 10:38:36 +0800

    add put method in frontend

--- frontend/src/util/api.js
+++ frontend/src/util/api.js
@@ -34,6 +34,23 @@ export const post = async (endpoint, body, token = null) => {
   return data;
 };
 
+export const put = async (endpoint, body, token = null) => {
+  const options = {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      ...(token && { Authorization: `Bearer ${token}` }),
+    },
+    body: JSON.stringify(body),
+  };
+  const response = await fetch(`${baseUrl}/${endpoint}`, options);
+  if (!response.ok) {
+    const error = await response.text();
+    throw new Error(error);
+  }
+  return;
+};
+
 export const delete_request = async (endpoint, token = null) => {
   const options = {
     method: 'DELETE',
